7daaaa657835d6dd1ba1c4e323d225eace7c5dbe
Gustavo Jung <gujung23@gmail.com>
2024-05-18 17:49:29 -0300

    uptade interface

--- interface/main.go
+++ interface/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
@@ -19,6 +20,28 @@ func main() {
 
 	Geometria(retangulo)
 	Geometria(circulo)
+
+	// Errors
+	Errors(errors.New("a error"))
+	p := ProblemaNetwork{
+		Rede:     true,
+		Hardware: false,
+	}
+	Errors(p)
+
+	//interface vazia
+	var lista []interface{}
+	lista = append(lista, 10)
+	lista = append(lista, 7.5)
+	lista = append(lista, true)
+	lista = append(lista, "teste")
+
+	for _, valor := range lista {
+		if v, ok := valor.(string); ok {
+			fmt.Println(v + " string")
+		}
+		fmt.Println(valor)
+	}
 
 }
 
@@ -44,4 +67,25 @@ func (c circulo) area() float64 {
 
 func Geometria(g geometria) {
 	fmt.Println("Area:", g.area())
+}
+
+// struct error
+type ProblemaNetwork struct {
+	Rede     bool
+	Hardware bool
+}
+
+func (p ProblemaNetwork) Error() string {
+	if p.Rede {
+		return "Problema de Rede"
+	} else if p.Hardware {
+		return "Problema de Hardware"
+	} else {
+		return "Outro Problema"
+	}
+}
+
+// func error
+func Errors(err error) {
+	fmt.Println(err.Error())
 }
