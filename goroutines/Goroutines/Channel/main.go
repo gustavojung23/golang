ce08eac0acb5cb916a5404875a02317c22474329
Gustavo Jung <gujung23@gmail.com>
2024-05-19 16:35:08 -0300

    Update chan

--- goroutines/Goroutines/Channel/main.go
+++ goroutines/Goroutines/Channel/main.go
@@ -1,22 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Channels")
 
-	channel := make(chan int)
+	channel := make(chan int, 100)
 
 	go setList(channel)
 
 	for v := range channel {
-		fmt.Println(v)
+		fmt.Println("Recebendo:", v)
+		time.Sleep(time.Second)
 	}
 }
 
 func setList(channel chan int) {
 	for i := 0; i < 100; i++ {
 		channel <- i
+		fmt.Println("Enviando:", i)
+		time.Sleep(time.Second)
 	}
 	close(channel)
 }
+
+// <-chan - leitura
+// chan<- escrita
+//channel := make(chan int, 100) - channel buffered
